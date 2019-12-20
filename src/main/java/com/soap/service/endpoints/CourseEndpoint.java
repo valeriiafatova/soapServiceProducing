dbadc02d785a882334d61dc4396bfa4265d976b8
Valeriia Fatova <valeriia_fatova@epam.com>
2019-12-20 15:58:58 +0200

    02 Configure Soap webservice

---
+++ src/main/java/com/soap/service/endpoints/CourseEndpoint.java
@@ -0,0 +1,62 @@
+package com.soap.service.endpoints;
+
+import com.soap.service.entity.Course;
+import com.soap.service.repository.CourseRepository;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import tutorial.soapservice.GetCourseRequest;
+import tutorial.soapservice.GetCourseResponse;
+import tutorial.soapservice.GetCourseResponse2;
+import tutorial.soapservice.ObjectFactory;
+
+import javax.annotation.Resource;
+import java.util.Optional;
+
+@Endpoint
+public class CourseEndpoint {
+    private static final String NAMESPACE_URI = "http://soapService.tutorial";
+    
+    @Resource
+    private CourseRepository courseRepository;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCourseRequest")
+    @ResponsePayload
+    public GetCourseResponse getCourse(@RequestPayload GetCourseRequest request){
+        ObjectFactory objectFactory = new ObjectFactory();
+        GetCourseResponse response = objectFactory.createGetCourseResponse();
+        
+        Optional<Course> courseByTitle = courseRepository.findCourseByTitle(request.getTitle());
+        if(courseByTitle.isPresent()){
+            Course courseFromRepo = courseByTitle.get();
+            tutorial.soapservice.Course course = objectFactory.createCourse();
+            course.setId(courseFromRepo.getId().intValue());
+            course.setTitle(courseFromRepo.getTitle());
+            response.setCourse(course);
+            return response;
+        }
+        //need to handle
+        return null;
+    }
+    
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCourseRequest2")
+    @ResponsePayload
+    public GetCourseResponse2 getCourse2(@RequestPayload GetCourseRequest request){
+        ObjectFactory objectFactory = new ObjectFactory();
+        GetCourseResponse2 response = objectFactory.createGetCourseResponse2();
+        
+        Optional<Course> courseByTitle = courseRepository.findCourseByTitle(request.getTitle());
+        if(courseByTitle.isPresent()){
+            Course courseFromRepo = courseByTitle.get();
+            tutorial.soapservice.Course course = objectFactory.createCourse();
+            course.setId(courseFromRepo.getId().intValue());
+            course.setTitle(courseFromRepo.getTitle());
+            response.setCourse2(course);
+            return response;
+        }
+        //need to handle
+        return null;
+    }
+    
+}
